fc453d5b4c59ccbd3c066f74bc6ad910ca733b94
xulihang <xulihanghai@163.com>
2021-11-23 01:08:05 -0800

    Initial Commit

---
+++ barcodesdkaggregator/barcodesdkaggregatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  barcodesdkaggregatorApp.swift
+//  barcodesdkaggregator
+//
+//  Created by xulihang on 2021/11/23.
+//
+
+import SwiftUI
+
+@main
+struct barcodesdkaggregatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
