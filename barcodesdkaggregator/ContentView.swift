48c229094b728428c9527069b38222e73d12098a
xulihang <xulihanghai@163.com>
2021-11-25 00:08:15 -0800

    add server

--- barcodesdkaggregator/ContentView.swift
+++ barcodesdkaggregator/ContentView.swift
@@ -10,16 +10,15 @@ import SwiftyJSON
 
 struct ContentView: View {
     @ObservedObject private var barcodeReader:Aggregator = Aggregator(name: "MLKit")
-    @State private var remoteImage : UIImage? = nil
-    let placeholderOne = UIImage(named: "dynamsoft")
+    @ObservedObject private var webServer:Server = Server()
     @State var status = "Not connected."
-    @State var json = ""
     @State private var selectedSDK = Aggregator.mlKit
     @State var currentImageURL = "http://192.168.8.65:5111/session/abc7b55641cf11eca240e84e068e29b8/image/QR1a.jpg"
     @State private var showAlert = false
     var body: some View {
         Spacer()
         VStack{
+            Text(status)
             HStack{
                 Text("Selected SDK: ")
                 Picker("SDK", selection: $selectedSDK) {
@@ -31,26 +30,30 @@ struct ContentView: View {
                 }
             }
 
-            TextField("Image URL", text: $currentImageURL)
-            Button("Local Test"){
-                Task  {
-                    await buttonPressed()
+            TextField("Image URL", text: $currentImageURL).textFieldStyle(.roundedBorder)
+            HStack{
+                Button(action: fetchRemoteImage) {
+                    Text("Fetch")
                 }
-            }.alert(isPresented: $showAlert) {
-                Alert(
-                    title: Text("No barcodes found"),
-                    message: Text("Try to decode with another SDK.")
-                )
+                Button("Local Test"){
+                    Task  {
+                        await decode()
+                    }
+                }.alert(isPresented: $showAlert) {
+                    Alert(
+                        title: Text("No barcodes found"),
+                        message: Text("Try to decode with another SDK.")
+                    )
+                }
             }
-
             
-            Text(status)
-            Image(uiImage: remoteImage ?? placeholderOne!)
+            
+            Image(uiImage: webServer.currentImage)
                 .resizable()
-                .aspectRatio((remoteImage ?? placeholderOne!).size, contentMode: .fit)
+                .aspectRatio((webServer.currentImage).size, contentMode: .fit)
                 .onAppear(perform: fetchRemoteImage)
             ScrollView(.vertical) {
-                Text(json)
+                Text(webServer.resultsString)
             }
             
             
@@ -66,36 +69,32 @@ struct ContentView: View {
     }
     
     func onViewLoaded(){
-        
+        webServer.passBarcodeReader(barcodeReader: barcodeReader)
+        status = "Started at: "+webServer.getServerURL()
     }
     
     func decode() async{
-        
-        let results:NSArray = await barcodeReader.decode(image: remoteImage!)
+        let results:NSArray = await barcodeReader.decode(image: webServer.currentImage)
         print(results.count)
         if results.count>0{
             let result: NSDictionary = results[0] as! NSDictionary
             print(result["barcodeText"] ?? "")
             let json = JSON(results)
-
             let representation = json.rawString(options: [])
-            print(representation!)
-            self.json = representation ?? ""
+            DispatchQueue.main.async {
+                webServer.resultsString = representation ?? ""
+            }
         }else{
             showAlert = true
         }
     }
     
-    func buttonPressed() async{
-        print("pressed")
-        fetchRemoteImage()
-        await decode()
-    }
-    
     func fetchRemoteImage(){
         let url = URL(string: self.currentImageURL)
         let data = try? Data(contentsOf: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
-        self.remoteImage = UIImage(data: data!)
+        DispatchQueue.main.async {
+            webServer.currentImage = UIImage(data: data!)!
+        }
     }
     
     func fetchRemoteImageAsync()
@@ -103,7 +102,7 @@ struct ContentView: View {
         guard let url = URL(string: self.currentImageURL) else { return }
         URLSession.shared.dataTask(with: url){ (data, response, error) in
             if let image = UIImage(data: data!){
-                self.remoteImage = image
+                webServer.currentImage = image
             }
             else{
                 print(error ?? "")
---
+++ barcodesdkaggregator/Server.swift
@@ -0,0 +1,57 @@
+//
+//  Server.swift
+//  barcodesdkaggregator
+//
+//  Created by xulihang on 2021/11/24.
+//
+
+import Foundation
+import GCDWebServer
+import SwiftyJSON
+
+class Server:ObservableObject{
+    private var webServer:GCDWebServer
+    private var barcodeReader:Aggregator! = nil
+    @Published var currentImage:UIImage = UIImage(named: "dynamsoft")!
+    @Published var resultsString:String = ""
+    init(){
+        webServer = GCDWebServer()
+        webServer.addDefaultHandler(forMethod: "GET", request: GCDWebServerRequest.self) { request in
+            return GCDWebServerDataResponse(html:"<html><body><p>Hello World</p></body></html>")
+        }
+        webServer.addDefaultHandler(forMethod: "POST", request: GCDWebServerDataRequest.self, asyncProcessBlock: {request,completion  in
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 5) {
+                let get = request as! GCDWebServerDataRequest
+                do{
+                    let json = try JSON(data: get.data)
+                    let imageBase64 = json["base64"]
+                    print(imageBase64)
+                    print(self.barcodeReader.name)
+                    let imageData = Data(base64Encoded: imageBase64.rawString() ?? "", options: .ignoreUnknownCharacters)
+                    self.currentImage = UIImage(data: imageData!)!
+                    Task() {
+                        do {
+                            let results = await self.barcodeReader.decode(image: self.currentImage)
+                            let json2 = JSON(results)
+                            self.resultsString = json2.rawString(options: []) ?? ""
+                            completion(GCDWebServerDataResponse(jsonObject: results))
+                        }
+                    }
+                } catch{
+                    print(error)
+                    completion(GCDWebServerErrorResponse(text: error.localizedDescription))
+                }
+            }
+        })
+        webServer.start(withPort: 8888, bonjourName: "GCD Web Server")
+    }
+    
+    func passBarcodeReader(barcodeReader:Aggregator){
+        self.barcodeReader = barcodeReader
+    }
+    
+    func getServerURL() -> String{
+        return webServer.serverURL?.absoluteString ?? ""
+    }
+    
+}
